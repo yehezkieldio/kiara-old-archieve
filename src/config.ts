69310779da5b9343ec9b2114d240dea2b610f829
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-02-12 07:46:51 +0800

    chore: remove configExists function

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,8 @@
 import type { KiaraConfig } from "#/types/config";
-import { join } from "node:path";
-import { currentWorkingDirectory } from "#/lib/constants";
-import { fileExists } from "#/lib/utils/file-exists";
 import { loadConfig } from "c12";
 
 const { config } = await loadConfig<KiaraConfig>({
     name: "kiara",
 });
-
-export async function configExists(): Promise<boolean> {
-    const extensions = [".js", ".ts", ".mjs", ".cjs", ".mts", ".cts", ".json"] as string[];
-    const files: string[] = extensions.map((ext: string): string => `kiara.config${ext}`);
-
-    for (const file of files) {
-        if (await fileExists(join(currentWorkingDirectory, file))) {
-            return true;
-        }
-    }
-
-    return false;
-}
 
 export default config;
