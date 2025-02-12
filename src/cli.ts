b82b27a501959d6e7e12dca0e6e3c18de479e7bf
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-02-12 19:06:57 +0800

    feat(cli): add short flags for name and skip-verify options

--- src/cli.ts
+++ src/cli.ts
@@ -21,10 +21,10 @@ program
     .description(internal.description!)
     .version(internal.version!, "-v, --version", "Output the current version of kiara.")
     .option("-V, --verbose", "Enable verbose logging for debugging purposes.", false)
-    .option("--name", "The name of the package to release, defaults to the name in package.json.")
+    .option("-n --name", "The name of the package to release, defaults to the name in package.json.")
     .option("-s --skip-bump", "Skip the version bump step, ideal for first-time releases.", false)
     .option("-d --dry-run", "Run kiara without making any changes to the repository.", false)
-    .option("-n --skip-verify", "Skip the verification step, useful for debugging.", false)
+    .option("-k --skip-verify", "Skip the verification step, useful for debugging.", false)
     .action(async (): Promise<void> => {
         logger.info(`Running kiara version ${color.dim(internal.version!)}`);
 
