cbfaae8dc6e19ef4cc94de9f89aa4d9a6b4594b5
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-02-15 02:39:53 +0800

    refactor: refactor verifyConditions to use sequential execution

--- src/tasks/verify-conditions.ts
+++ src/tasks/verify-conditions.ts
@@ -1,11 +1,12 @@
-import { ResultAsync, okAsync } from "neverthrow";
+import { type ResultAsync, okAsync } from "neverthrow";
 import type { KiaraContext } from "#/kiara";
 import { logger } from "#/libs/logger";
 import { preflightEnvironment } from "#/tasks/preflight-environment";
 import { preflightGit } from "#/tasks/preflight-git";
 
 export function verifyConditions(context: KiaraContext): ResultAsync<void, Error> {
-    return ResultAsync.combine([preflightEnvironment(context), preflightGit(context)])
+    return preflightEnvironment(context)
+        .andThen((): ResultAsync<void, Error> => preflightGit(context))
         .andTee((): void => logger.info("All preflight checks passed."))
         .andThen((): ResultAsync<undefined, never> => okAsync(undefined));
 }
