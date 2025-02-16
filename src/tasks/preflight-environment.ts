85f9e6d455bc7adc89459aeee51fb32f9dd922e6
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-02-16 11:41:51 +0800

    feat: add preflight checks for environment and git

--- src/tasks/preflight-environment.ts
+++ src/tasks/preflight-environment.ts
@@ -1,6 +1,25 @@
-import { type ResultAsync, okAsync } from "neverthrow";
+import { type Result, type ResultAsync, err, errAsync, ok, okAsync } from "neverthrow";
 import type { KiaraContext } from "#/kiara";
+import { CWD_PACKAGE_PATH } from "#/libs/constants";
+import { fileExists } from "#/libs/file";
+import { logger } from "#/libs/logger";
 
 export function preflightEnvironment(context: KiaraContext): ResultAsync<KiaraContext, Error> {
-    return okAsync(context);
+    return checkToken(context).asyncAndThen(checkPackageJson);
+}
+
+function checkToken(context: KiaraContext): Result<KiaraContext, Error> {
+    if (!context.options.token && !process.env.GITHUB_TOKEN && !process.env.GH_TOKEN) {
+        return err(new Error("GitHub token is required."));
+    }
+
+    return ok(context);
+}
+
+function checkPackageJson(context: KiaraContext): ResultAsync<KiaraContext, Error> {
+    return fileExists(CWD_PACKAGE_PATH)
+        .andTee(() => logger.verbose("Checking for package.json file: fileExists"))
+        .andThen((exists) => {
+            return exists ? okAsync(context) : errAsync(new Error("package.json file not found."));
+        });
 }
--- src/tasks/preflight-git.ts
+++ src/tasks/preflight-git.ts
@@ -1,6 +1,71 @@
-import { type ResultAsync, okAsync } from "neverthrow";
+import type { Octokit } from "@octokit/core";
+import { execa } from "execa";
+import { ResultAsync, errAsync, okAsync } from "neverthrow";
 import type { KiaraContext } from "#/kiara";
+import { createOctokit } from "#/libs/github";
+import { logger } from "#/libs/logger";
 
 export function preflightGit(context: KiaraContext): ResultAsync<KiaraContext, Error> {
-    return okAsync(context);
+    return checkGitRepository(context)
+        .andThen(checkUncommittedChanges)
+        .andThen(checkGithubToken)
+        .andThen(checkUpstreamBranch);
+}
+
+function checkGitRepository(context: KiaraContext): ResultAsync<KiaraContext, Error> {
+    return ResultAsync.fromPromise(
+        execa("git", ["rev-parse", "--is-inside-work-tree"], { cwd: process.cwd() }),
+        (error: unknown): Error => new Error(`Error checking for git repository: ${error}`)
+    )
+        .andTee(({ command }) => logger.verbose(`Checking for git repository: ${command}`))
+        .andThen((result): ResultAsync<KiaraContext, Error> => {
+            return result.stdout === "true"
+                ? okAsync(context)
+                : errAsync(new Error("Could not find a git repository in the current working directory."));
+        });
+}
+
+function checkUncommittedChanges(context: KiaraContext): ResultAsync<KiaraContext, Error> {
+    return ResultAsync.fromPromise(
+        execa("git", ["status", "--porcelain"], { cwd: process.cwd() }),
+        (error) => new Error(`Error checking for uncommitted changes: ${error}`)
+    )
+        .andTee(({ command }): void => logger.verbose(command))
+        .andThen((result): ResultAsync<KiaraContext, Error> => {
+            return result.stdout === ""
+                ? okAsync(context)
+                : errAsync(new Error("There are uncommitted changes in the current working directory."));
+        });
+}
+
+function checkGithubToken(context: KiaraContext): ResultAsync<KiaraContext, Error> {
+    const octokit: ResultAsync<Octokit, unknown> = createOctokit(context.options.token || process.env.GITHUB_TOKEN);
+
+    return octokit
+        .andThen((client: Octokit): ResultAsync<KiaraContext, Error> => {
+            return ResultAsync.fromPromise(
+                client.request("GET /user"),
+                (error: unknown): Error => new Error(`Error checking GitHub token: ${error}`)
+            )
+                .andTee(({ url }) => logger.verbose(`Checking token with GitHub API: ${url}`))
+                .andThen((): ResultAsync<KiaraContext, never> => okAsync(context));
+        })
+        .orElse((): ResultAsync<never, Error> => {
+            return errAsync(new Error("The GitHub token is invalid or does not have the required permissions."));
+        });
+}
+
+function checkUpstreamBranch(context: KiaraContext): ResultAsync<KiaraContext, Error> {
+    return ResultAsync.fromPromise(
+        execa("git", ["rev-parse", "--abbrev-ref", "--symbolic-full-name", "@{u}"]),
+        (error) => new Error(`Error checking upstream branch: ${error}`)
+    )
+        .andTee(({ command }) => logger.verbose(`Checking upstream branch: ${command}`))
+        .andThen((result) => {
+            return result.stdout !== ""
+                ? okAsync(context)
+                : errAsync(
+                      new Error("No upstream branch found. Please set an upstream branch before running this command.")
+                  );
+        });
 }
