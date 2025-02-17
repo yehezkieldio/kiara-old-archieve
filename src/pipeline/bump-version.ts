a7f6929633eeeedce94bccdc8052f6100987ca4d
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-02-17 17:53:16 +0800

    feat: bump package version

--- src/pipeline/bump-version.ts
+++ src/pipeline/bump-version.ts
@@ -1,11 +1,33 @@
 import { type ResultAsync, okAsync } from "neverthrow";
 import type { KiaraContext } from "#/kiara";
+import { CWD_PACKAGE_PATH } from "#/libs/const";
 import { color, logger } from "#/libs/logger";
+import { updatePackageVersion } from "#/libs/package-json";
 
 export function bumpVersion(context: KiaraContext): ResultAsync<KiaraContext, Error> {
+    if (context.version.current === context.version.new) {
+        logger.info(
+            `No version bump required. Current version is ${color.dim(context.version.current)}.`
+        );
+        return okAsync(context);
+    }
+
+    if (context.options.skipBump) {
+        logger.info(
+            `Skipping version bump. Using current version ${color.dim(context.version.current)}.`
+        );
+        return okAsync(context);
+    }
+
     logger.info(
         `Bumping version from ${color.dim(context.version.current)} to ${color.dim(context.version.new)}`
     );
 
-    return okAsync(context);
+    if (context.options.dryRun) {
+        return okAsync(context);
+    }
+
+    return updatePackageVersion(CWD_PACKAGE_PATH, context.version.new).map(
+        (): KiaraContext => context
+    );
 }
