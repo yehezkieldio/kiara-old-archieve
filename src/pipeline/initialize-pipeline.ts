5af84b11d8fd12962b329380d44d9776ba89fde2
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-02-17 11:32:39 +0800

    feat: implement full release pipeline

--- src/pipeline/initialize-pipeline.ts
+++ src/pipeline/initialize-pipeline.ts
@@ -3,8 +3,12 @@ import type { ResultAsync } from "neverthrow";
 import type { KiaraOptions } from "#/kiara";
 import { color, logger } from "#/libs/logger";
 import { INTERNAL } from "#/libs/util/internal";
+import { bumpVersion } from "#/pipeline/bump-version";
 import { createContext } from "#/pipeline/create-context";
+import { createTagAndCommit } from "#/pipeline/create-tag-and-commit";
+import { generateChangelog } from "#/pipeline/generate-changelog";
 import { pushAndRelease } from "#/pipeline/push-and-release";
+import { selectBumpStrategy } from "#/pipeline/select-bump-strategy";
 import { verifyConditions } from "#/pipeline/verify-conditions";
 
 export function initializePipeline(options: KiaraOptions): ResultAsync<void, Error> {
@@ -13,11 +17,16 @@ export function initializePipeline(options: KiaraOptions): ResultAsync<void, Err
     logger.info(`Running ${color.magenta("kiara")} version ${color.dim(INTERNAL.VERSION)}`);
     logger.verbose(`Options: ${JSON.stringify(options)}`);
 
-    return createContext(options)
+    const pipeline: ResultAsync<void, Error> = createContext(options)
         .andThen(verifyConditions)
-        .andThen(pushAndRelease)
-        .mapErr((error: Error): Error => {
-            logger.error(error.message);
-            process.exit(1);
-        });
+        .andThen(selectBumpStrategy)
+        .andThen(bumpVersion)
+        .andThen(generateChangelog)
+        .andThen(createTagAndCommit)
+        .andThen(pushAndRelease);
+
+    return pipeline.mapErr((error: Error): Error => {
+        logger.error(error.message);
+        process.exit(1);
+    });
 }
